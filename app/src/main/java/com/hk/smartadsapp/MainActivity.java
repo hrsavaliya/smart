a09ba978017ef42863ea9e0006204025648fdb0d
herry <herry@owner.com>
2020-12-19 18:27:17 +0530

    test commit

--- app/src/main/java/com/hk/smartadsapp/MainActivity.java
+++ app/src/main/java/com/hk/smartadsapp/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     SmartInterstitialPreload smartInterstitialPreload;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
